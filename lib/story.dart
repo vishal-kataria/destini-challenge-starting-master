ac139976222dc00a5d46053ef649c6d24887f6e7
vishal kataria <vk@vishals-MacBook-Pro.local>
2020-04-17 18:16:42 +0530

    Initial commit

---
+++ lib/story.dart
@@ -0,0 +1,9 @@
+class Story {
+  String title;
+  String choice1, choice2;
+  Story({String storyTitle, String choice1, String choice2}) {
+    title = storyTitle;
+    this.choice1 = choice1;
+    this.choice2 = choice2;
+  }
+}
